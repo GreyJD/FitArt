330d1169a8507bb83fecc1954ca3305fe2e7b0b0
Jacob Blechel <JTBlechel@gmail.com>
2020-09-30 10:47:17 -0700

    added service code
    need a small icon, when an app runs in the background a small icon is displayed so the user knows about it, there isn't one in res/drawable file so i've left it out for now.

--- FitArt_AndroidStudio/app/src/main/java/com/example/fitart/MapsActivity.java
+++ FitArt_AndroidStudio/app/src/main/java/com/example/fitart/MapsActivity.java
@@ -1,8 +1,14 @@
 package com.example.fitart;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.IBinder;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,6 +20,49 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    /*service code, this can be moved later, I am just assuming this will be the main activity
+    * */
+
+    // service code used from
+    // https://stackoverflow.com/questions/34573109/how-to-make-an-android-app-to-always-run-in-background
+    public class getLocationService extends Service {
+
+        private static final int NOTIF_ID = 1;
+        private static final String NOTIF_CHANNEL_ID = "CHANNEL_ID";
+
+        @Nullable
+        @Override
+        public IBinder onBind(Intent intent){ return null; }
+
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId){
+            //do work here
+            startForeground();
+            return super.onStartCommand(intent, flags, startId);
+        }
+
+        private void startForeground(){
+            Intent notificationIntent = new Intent(this, /**/MapsActivity.class/**/); //must alter if this moves to a new activity
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+            //NotificationCompat is for old versions, maybe update to Notification.builder? needs research
+            startForeground(NOTIF_ID, new NotificationCompat.Builder(this,NOTIF_CHANNEL_ID)
+            .setOngoing(true)
+            //.setSmallIcon(R.drawable.ic_notification) //this is found in res/drawable... there is no icon so it gives an error
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText("service running in background")
+            .setContentIntent(pendingIntent)
+            .build());
+
+            
+        }
+
+
+    }
+
+
+    /*end service code*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
