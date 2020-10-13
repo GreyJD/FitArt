bd4bbe652754dbf57085f5d7cdfe9f9c08d2c344
Jacob Blechel <JTBlechel@gmail.com>
2020-10-12 19:26:51 -0700

    This is what i want to work
    location objects are parcelable, meaning they can easily be added to an intent and broadcasted. Arraylists of LatLng are not by default, There's a workaround, but i don't know how it works yet

--- FitArt_AndroidStudio/app/src/main/java/com/example/fitart/BackgroundGPSReceiver.java
+++ FitArt_AndroidStudio/app/src/main/java/com/example/fitart/BackgroundGPSReceiver.java
@@ -6,18 +6,20 @@ import android.content.Intent;
 import android.location.Location;
 import android.widget.Button;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
 public class BackgroundGPSReceiver extends BroadcastReceiver{
 
     //accepts gps data from background service
 
-        Location lastReceivedLocation;
+        ArrayList<LatLng> lastReceivedLocation;
 
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent.getAction() == "GET_LOCATION_IN_BACKGROUND"){
                lastReceivedLocation = intent.getParcelableExtra("LOCATION");
-
-
             }
         }
     }
