85b1438151cb74d8e0c6395fe25e0f4e728203d7
Jacob Blechel <JTBlechel@gmail.com>
2020-10-23 12:05:38 -0700

    Revert "Merge branch 'master' into Blechel"
    This reverts commit ad46181fc0dcd9b7b44c3bb6057d1f41492b43f1, reversing
    changes made to 9b08e47d901d89ee7cacf1dc0bd3b55036107b19.

--- FitArt_AndroidStudio/app/src/main/java/com/example/fitart/MapsActivity.java
+++ FitArt_AndroidStudio/app/src/main/java/com/example/fitart/MapsActivity.java
@@ -7,13 +7,10 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -67,23 +64,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-  
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                //startSettings();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+
 }
