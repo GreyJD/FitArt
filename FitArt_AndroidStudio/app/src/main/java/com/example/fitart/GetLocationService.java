28373901cec4c980dbf29c161424f91222cda2b1
Jacob Blechel <JTBlechel@gmail.com>
2020-10-23 12:04:22 -0700

    Revert "Merge pull request #12 from GreyJD/Blechel"
    This reverts commit 28f3b0408a02bb6f6dce1ca7922cc883a659976d, reversing
    changes made to 22c5e5b347879cd8697bbd4d09fdbfa3e0841cf9.

--- FitArt_AndroidStudio/app/src/main/java/com/example/fitart/GetLocationService.java
+++ FitArt_AndroidStudio/app/src/main/java/com/example/fitart/GetLocationService.java
@@ -15,18 +15,13 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 
-import com.google.android.gms.maps.model.LatLng;
-
-import java.util.ArrayList;
-
 public class GetLocationService extends Service {
+
 
     // service code used from
     // https://stackoverflow.com/questions/34573109/how-to-make-an-android-app-to-always-run-in-background
     private static final int NOTIF_ID = 1;
-    private static final String NOTIF_CHANNEL_ID = "GetLocationServiceChannel"; // add once channel is in place
-
-    ArrayList<LatLng> locationList;
+    private static final String NOTIF_CHANNEL_ID = "Channel_Id"; // add once channel is in place
 
     @Nullable
     @Override
@@ -35,10 +30,9 @@ public class GetLocationService extends Service {
     }
 
     @Override
-    public void onCreate(){
-        super.onCreate();
+    public int onStartCommand(Intent intent, int flags, int startId) {
 
-        locationList = new ArrayList<LatLng>();
+        //do location stuff here
 
         LocationManager locationManager;
         LocationListener locationListener;
@@ -47,57 +41,44 @@ public class GetLocationService extends Service {
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                //add each location to LocationList
-                LatLng mostRecent = new LatLng(location.getLatitude(),location.getLongitude());
-                locationList.add(mostRecent);
+                //send location to whatever needs it, maybe a workManager?
             }
 
             @Override
             public void onStatusChanged(String s, int i, Bundle bundle) {
+
             }
 
             @Override
             public void onProviderEnabled(String s) {
                 // maybe add some sort of screen animation if we're feelin' spicy
+
             }
 
             @Override
             public void onProviderDisabled(String s) {
                 // maybe add some sort of screen animation if we're feelin' spicy
+
             }
         };
+
 
         if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
 
             //print permission denied IMPORTANT: the module that starts this service should make this check again
             // and if true then request permissions, this can only be done in an activity, not a service.
             //must use requestPermissions for access_fine, acess_coarse, and internet
         }
-        locationManager.requestLocationUpdates("gps", 25000, 0, locationListener);
 
-    }
-
-    @Override
-    public void onDestroy(){
-
-        broadcastLocationList();
-        super.onDestroy();
-        // !! location manager/listener needs to be deallocated here to avoid mem leak !!
-
-    }
+        locationManager.requestLocationUpdates("gps", 25000, 10, locationListener);
 
 
-    @Override
-    public int onStartCommand(final Intent intent, int flags, int startId) {
-        // This function should be used to return location data.
-        startForeground(); // this could be moved to onCreate for optimization
-
-        broadcastLocationList();
-
-        return START_NOT_STICKY;
+        startForeground();
+        return super.onStartCommand(intent, flags, startId);
     }
 
     private void startForeground(){
@@ -106,26 +87,12 @@ public class GetLocationService extends Service {
         //NotificationCompat is for old versions, maybe update to Notification.builder? needs research
         startForeground(NOTIF_ID, new NotificationCompat.Builder(this,NOTIF_CHANNEL_ID)
                 .setOngoing(true)
-                .setSmallIcon(R.drawable.pototype_small_icon) //this is found in res/drawable... there is no icon so it gives an error
+                //.setSmallIcon(R.drawable.ic_notification) //this is found in res/drawable... there is no icon so it gives an error
                 .setContentTitle(getString(R.string.app_name))
                 .setContentText("service running in background")
                 .setContentIntent(pendingIntent)
                 .build());
     }
 
-    private void broadcastLocationList(){
-        if(!locationList.isEmpty()) {
-            Intent sendLocation = new Intent();
-            sendLocation.putParcelableArrayListExtra("LOCATION", locationList);
-            sendLocation.setAction("GET_LOCATION_IN_BACKGROUND");
-            sendBroadcast(sendLocation);
-
-            locationList = new ArrayList<LatLng>();
-            // I believe reference to the old arraylist is attached to the broadcasted intent so no memory is
-            // leaked here, but I'm more of a cpp guy so this needs a closer look
-        }
-    }
 
 }
-
-
