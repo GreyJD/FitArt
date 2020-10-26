5b8affbf955ff985a908337c6381258b1c598275
Jacob Blechel <JTBlechel@gmail.com>
2020-10-26 12:03:27 -0700

    Revert "Revert "Merge branch 'master' into Blechel""
    This reverts commit 85b1438151cb74d8e0c6395fe25e0f4e728203d7.

--- FitArt_AndroidStudio/app/src/main/java/com/example/fitart/MapRecordingActivity.java
+++ FitArt_AndroidStudio/app/src/main/java/com/example/fitart/MapRecordingActivity.java
@@ -1,5 +1,6 @@
 package com.example.fitart;
 
+
 import android.Manifest;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -16,17 +17,42 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.maps.MapFragment;
 
+
+
+
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.telephony.IccOpenLogicalChannelResponse;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
+
 public class MapRecordingActivity extends AppCompatActivity
 {
     private boolean playPauseButtonClicked = false;
     private int mapArtButtonClicked = 0;
     BackgroundGPSReceiver backgroundGPSReceiver;
 
+    private static SeekBar seek_bar;
+    ImageButton colorButton;
+    ImageView colorSwatchImage;
+    int defaultColor;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_recording);
+
 
         //sets up listener for broadcasts. moves gps data from service to activity
         backgroundGPSReceiver = new BackgroundGPSReceiver();
@@ -129,6 +155,74 @@ public class MapRecordingActivity extends AppCompatActivity
         />
 
   */
+
+        //Find Variables
+        colorButton = findViewById(R.id.button_color);
+        colorSwatchImage = findViewById(R.id.image_colorSwatch);
+        seekbar();
+
+        //Assign default color
+        defaultColor = ContextCompat.getColor(MapRecordingActivity.this, R.color.colorPrimary);
+
+        //On.click.listener for color palette
+        colorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openColorPicker();
+            }
+        });
+    }
+
+    //Brush Size Controls
+    public void seekbar()
+    {
+        seek_bar = (SeekBar)findViewById(R.id.seekBar_BrushSize);
+        seek_bar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    int brush_Size;
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        brush_Size = progress / 10;
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        //Debug purposes
+                        Toast.makeText(MapRecordingActivity.this, "size:" + brush_Size, Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+
+    //Color choice popup
+    public void openColorPicker()
+    {
+        AmbilWarnaDialog ambilWarnaDialog = new AmbilWarnaDialog(this, defaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                defaultColor = color;
+
+                //Attempt to update Color Swatch...Failed
+                PorterDuff.Mode mMode = PorterDuff.Mode.SRC_ATOP;
+                colorSwatchImage.setBackgroundColor(defaultColor);
+
+                //Debug purposes
+                Toast.makeText(MapRecordingActivity.this, "color:" + defaultColor, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        ambilWarnaDialog.show();
+
     }
 }
 
