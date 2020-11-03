4e95eb1035f3784dbc3e646719d13464b4fba2e3
Jacob Blechel <JTBlechel@gmail.com>
2020-11-03 15:19:23 -0800

    Revert "Revert "added sams classes. No issues""
    This reverts commit e6231eb138fd4efeea6bffdac6a911e5e1d60c9f.

--- FitArt_AndroidStudio/app/src/main/java/com/example/fitart/MapsActivity.java
+++ FitArt_AndroidStudio/app/src/main/java/com/example/fitart/MapsActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.widget.Toolbar;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import android.view.Menu;
@@ -18,8 +20,15 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.Serializable;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
     //@BindView(R.id.toolbar)
@@ -84,6 +93,180 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+}
+
+
+
+class PolyLineData implements Serializable {
+
+    private double startlat;
+    private double startlong;
+    private double endlat;
+    private double endlong;
+
+
+
+    public PolyLineData() {
+    }
+
+    public PolyLineData(LatLng startlocation, LatLng endlocation) {
+        this.startlat = startlocation.latitude;
+        this.startlong = startlocation.longitude;
+        this.endlat = endlocation.latitude;
+        this.endlong = endlocation.longitude;    }
+
+    public LatLng getStartlocation() {
+        return  new LatLng(startlat,startlong);
+    }
+
+    public void setStartlocation(LatLng startlocation) {
+        this.startlat = startlocation.latitude;
+        this.startlong = startlocation.longitude;
+    }
+
+    public LatLng getEndlocation() {
+        return  new LatLng(endlat,endlong);
+    }
+
+    public void setEndlocation(LatLng endlocation) {
+        this.endlat = endlocation.latitude;
+        this.endlong = endlocation.longitude;     }
+}
+
+
+class  MapStateManager {
+
+    private static final String LONGITUDE = "longitude";
+    private static final String LATITUDE = "latitude";
+    private static final String ZOOM = "zoom";
+    private static final String BEARING = "bearing";
+    private static final String TILT = "tilt";
+    private static final String MAPTYPE = "MAPTYPE";
+    private static final String POLYLINES = "polylines";
+
+
+
+    private ArrayList<PolyLineData> polyLineList;
+
+    private SharedPreferences mapStatePrefs;
+
+    public ArrayList<PolyLineData> getPolyLineList(){
+        return polyLineList;
+    }
+
+    public void addToPolyLineList(ArrayList<PolyLineData> value){
+        polyLineList = value;
+    }
+
+    public void loadPolyListFromState(){
+        Gson gson = new Gson();
+        String json = mapStatePrefs.getString(POLYLINES, null);
+        Type type = new TypeToken<ArrayList<PolyLineData>>() {}.getType();
+        polyLineList = gson.fromJson(json, type);
+        if(polyLineList ==  null)
+            polyLineList = new ArrayList<>();
+
+
+    }
+
+    public MapStateManager(Context context, String name) {
+        mapStatePrefs = context.getSharedPreferences(name, Context.MODE_PRIVATE);
+        polyLineList = new ArrayList<>();
+    }
+
+    public void saveMapState(GoogleMap mapMie) {
+        SharedPreferences.Editor editor = mapStatePrefs.edit();
+        CameraPosition position = mapMie.getCameraPosition();
+
+        Gson gson = new Gson();
+        String json = gson.toJson(polyLineList);
+        editor.putString(POLYLINES, json);
+
+        editor.putFloat(LATITUDE, (float) position.target.latitude);
+        editor.putFloat(LONGITUDE, (float) position.target.longitude);
+        editor.putFloat(ZOOM, position.zoom);
+        editor.putFloat(TILT, position.tilt);
+        editor.putFloat(BEARING, position.bearing);
+        editor.putInt(MAPTYPE, mapMie.getMapType());
+        editor.commit();
+    }
+
+    public CameraPosition getSavedCameraPosition() {
+        double latitude = mapStatePrefs.getFloat(LATITUDE, 0);
+        if (latitude == 0) {
+            return null;
+        }
+        double longitude = mapStatePrefs.getFloat(LONGITUDE, 0);
+        LatLng target = new LatLng(latitude, longitude);
+
+        float zoom = mapStatePrefs.getFloat(ZOOM, 0);
+        float bearing = mapStatePrefs.getFloat(BEARING, 0);
+        float tilt = mapStatePrefs.getFloat(TILT, 0);
+
+        CameraPosition position = new CameraPosition(target, zoom, tilt, bearing);
+        return position;
+    }
+
+    public int getSavedMapType() {
+        return mapStatePrefs.getInt(MAPTYPE, GoogleMap.MAP_TYPE_NORMAL);
+    }
+}
+
+class KalmanLatLong {
+    private final float MinAccuracy = 1;
+
+    private float Q_metres_per_second;
+    private long TimeStamp_milliseconds;
+    private double lat;
+    private double lng;
+    private float variance; // P matrix.  Negative means object uninitialised.  NB: units irrelevant, as long as same units used throughout
+
+    public KalmanLatLong(float Q_metres_per_second) { this.Q_metres_per_second = Q_metres_per_second; variance = -1; }
+
+    public long get_TimeStamp() { return TimeStamp_milliseconds; }
+    public double get_lat() { return lat; }
+    public double get_lng() { return lng; }
+    public float get_accuracy() { return (float)Math.sqrt(variance); }
+
+    public void SetState(double lat, double lng, float accuracy, long TimeStamp_milliseconds) {
+        this.lat=lat; this.lng=lng; variance = accuracy * accuracy; this.TimeStamp_milliseconds=TimeStamp_milliseconds;
+    }
+
+    /// <summary>
+    /// Kalman filter processing for lattitude and longitude
+    /// </summary>
+    /// <param name="lat_measurement_degrees">new measurement of lattidude</param>
+    /// <param name="lng_measurement">new measurement of longitude</param>
+    /// <param name="accuracy">measurement of 1 standard deviation error in metres</param>
+    /// <param name="TimeStamp_milliseconds">time of measurement</param>
+    /// <returns>new state</returns>
+    public void Process(double lat_measurement, double lng_measurement, float accuracy, long TimeStamp_milliseconds) {
+        if (accuracy < MinAccuracy) accuracy = MinAccuracy;
+        if (variance < 0) {
+            // if variance < 0, object is unitialised, so initialise with current values
+            this.TimeStamp_milliseconds = TimeStamp_milliseconds;
+            lat=lat_measurement; lng = lng_measurement; variance = accuracy*accuracy;
+        } else {
+            // else apply Kalman filter methodology
+
+            long TimeInc_milliseconds = TimeStamp_milliseconds - this.TimeStamp_milliseconds;
+            if (TimeInc_milliseconds > 0) {
+                // time has moved on, so the uncertainty in the current position increases
+                variance += TimeInc_milliseconds * Q_metres_per_second * Q_metres_per_second / 1000;
+                this.TimeStamp_milliseconds = TimeStamp_milliseconds;
+                // TO DO: USE VELOCITY INFORMATION HERE TO GET A BETTER ESTIMATE OF CURRENT POSITION
+            }
+
+            // Kalman gain matrix K = Covarariance * Inverse(Covariance + MeasurementVariance)
+            // NB: because K is dimensionless, it doesn't matter that variance has different units to lat and lng
+            float K = variance / (variance + accuracy * accuracy);
+            // apply K
+            lat += K * (lat_measurement - lat);
+            lng += K * (lng_measurement - lng);
+            // new Covarariance  matrix is (IdentityMatrix - K) * Covarariance
+            variance = (1 - K) * variance;
         }
     }
 }
